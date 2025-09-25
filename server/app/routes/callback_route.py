21b9a6b98c73c3cc15b8c59b2db75e7feb951e68
John Nguyen <nguyenj1863@gmail.com>
2025-09-25 19:40:36 -0400

    feat: capture params and handle error

--- server/app/routes/callback_route.py
+++ server/app/routes/callback_route.py
@@ -1,7 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 callback_bp = Blueprint('callback', __name__)
 
-@callback_bp.route('/callback')
+@callback_bp.route('/callback', methods=['POST', 'GET'])
 def callback():
+    code = request.args.get('code')
+    state = request.args.get('state')
+    error = request.args.get('error')
+    if(error):
+        return f"Error: {error}", 400
     return "Callback endpoint"
