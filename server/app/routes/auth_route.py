680ccdafa0e9037c4ddfe6ed7f6a654e99e252b4
John Nguyen <nguyenj1863@gmail.com>
2025-09-25 19:28:11 -0400

    fix: rid of get since it's default method

--- server/app/routes/auth_route.py
+++ server/app/routes/auth_route.py
@@ -3,7 +3,7 @@ from services.auth_service import get_auth_url
 
 authorize_bp = Blueprint('auth', __name__)
 
-@authorize_bp.route('/auth', methods=['GET'])
+@authorize_bp.route('/auth')
 def authorize():
     auth_url = get_auth_url()
     return auth_url
