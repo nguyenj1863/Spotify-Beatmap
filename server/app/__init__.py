1ef0a1a5bb4671030c9cb72f5e4a6ef39a323225
John Nguyen <nguyenj1863@gmail.com>
2025-09-25 20:10:42 -0400

    chore: add secret key for session

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,10 +1,12 @@
 # server/app/__init__.py
 from flask import Flask
 from flask_cors import CORS
+import os
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
+    app.secret_key = os.getenv("SECRET_KEY")
     
     from app.routes import init_routes
     init_routes(app)
