f8e2a45019a422a5c8750da9a85cad3c5561e57b
John Nguyen <nguyenj1863@gmail.com>
2025-09-25 20:11:10 -0400

    chore: send user to spotify auth page

--- server/app/routes/auth_route.py
+++ server/app/routes/auth_route.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, redirect
 from app.services.auth_service import get_auth_url
 
 authorize_bp = Blueprint('auth', __name__)
@@ -6,4 +6,4 @@ authorize_bp = Blueprint('auth', __name__)
 @authorize_bp.route('/auth')
 def authorize():
     auth_url = get_auth_url()
-    return auth_url
+    return redirect(auth_url)
