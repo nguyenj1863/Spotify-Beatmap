68290cbcd53563da9a5bd5e35993204d3911596f
John Nguyen <nguyenj1863@gmail.com>
2025-09-28 14:29:05 -0400

    feat: add essential const for spotify (including url's and env)

---
+++ src/lib/spotify.ts
@@ -0,0 +1,5 @@
+const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
+const REDIRECT_URI = process.env.SPOTIFY_REDIRECT_URI;
+
+export const AUTH_URL = 'https://accounts.spotify.com/authorize';
+export const TOKEN_URL = 'https://accounts.spotify.com/api/token';
