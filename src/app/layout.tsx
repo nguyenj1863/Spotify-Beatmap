f02c0b3d4a1e9f16f95270ed19bce0349d3a80d2
John Nguyen <nguyenj1863@gmail.com>
2025-10-07 22:32:38 -0400

    fix: rename title to Spotify BeatMap (may change)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin']});
 
 export const metadata = {
-  title: "Spotify Analyst",
+  title: "Spotify BeatMap",
   description: "Discover your listening habits and future favorites",
   icons: {
     icon: '/favicon.ico',
