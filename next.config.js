de35e85b8f5d4084f3fb4953750cb82f8d4fa746
John Nguyen <nguyenj1863@gmail.com>
2025-10-08 19:42:29 -0400

    chore: allow spotify's images from i.scdn.co to be whitelisted

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.scdn.co",
+        port: "",
+        pathname: "/**", // allow all Spotify image paths
+      },
+    ],
+  },
+};
+
+export default nextConfig;
